2e8b28bbf2f746c4fc2fc4c8c60aef940a06a67d
lonzomagill <lonzomagill@gmail.com>
2023-01-22 14:31:22 +0800

    submit mul example

---
+++ com/chris/learn/MulExample.java
@@ -0,0 +1,13 @@
+package com.chris.learn;
+public class MulExample {
+
+    public static void main(String[] args) {
+        int m = 23;
+        int n = 22;
+        System.out.println("m * n = " + mul(m, n));
+    }
+
+    public static int mul (int a, int b) {
+        return a * b;
+    }
+}
