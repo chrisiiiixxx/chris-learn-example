3cac5c8699f38372f1ec2a933edeaf309a5d8b35
chris <chrisiiiixxx@gmail.com>
2023-02-12 14:03:03 +0800

    submit example

---
+++ com/chris/learn/NewExmaple.java
@@ -0,0 +1,26 @@
+package com.chris.learn;
+
+public class NewExmaple {
+    public static void main(String args[]){
+
+        String str = "www-youjsss-com";
+        String[] temp;
+        String delimeter = "-";  // 指定分割字符
+        temp = str.split(delimeter); // 分割字符串
+        // 普通 for 循环
+        for(int i =0; i < temp.length ; i++){
+            System.out.println(temp[i]);
+            System.out.println("");
+        }
+
+        System.out.println("------java for each循环输出的方法ss-----");
+        String str1 = "123";
+        String[] temp1;
+        String delimeter1 = "\\.";  // 指定分割字符， . 号需要转义
+        temp1 = str1.split(delimeter1); // 分割字符串
+        for(String x :  temp1){
+            System.out.println(x);
+            System.out.println("");
+        }
+    }
+}
